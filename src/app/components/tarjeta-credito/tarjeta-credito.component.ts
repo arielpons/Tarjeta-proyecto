dc5d810d73c965c442f09b68baffd8b672423064
Ariel <ariel_pons@hotmail.com>
2021-12-02 12:17:16 -0300

    Agregar Tarjeta Form

--- src/app/components/tarjeta-credito/tarjeta-credito.component.ts
+++ src/app/components/tarjeta-credito/tarjeta-credito.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-tarjeta-credito',
@@ -10,6 +11,29 @@ export class TarjetaCreditoComponent implements OnInit {
     { titular: 'Juan Perez', numeroTarjeta: '25252526262', fechaExpiracion: '11/23', cvv: '123' },
     { titular: 'Miguel Gonzalez', numeroTarjeta: '252526262', fechaExpiracion: '11/24', cvv: '312' },
   ];
-  constructor() { }
+  form: FormGroup;
 
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: [''],
+      numeroTarjeta: [''],
+      fechaExpiracion: [''],
+      cvv: ['']
+    })
+   }
+   
+   ngOnInit(): void {
+  }
+  
+   agregarTarjeta() {
+
+    const tarjeta: any = {
+      titular: this.form.get('titular')?.value,
+      numeroTarjeta: this.form.get('numeroTarjeta')?.value,
+      fechaExpiracion: this.form.get('fechaExpiracion')?.value,
+      cvv: this.form.get('cvv')?.value,
+    }
+    this.listTarjetas.push(tarjeta)
+    this.form.reset();
+  }
 }
